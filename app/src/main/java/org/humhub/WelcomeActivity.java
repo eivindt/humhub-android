73b2a81c57ca2ec6d8e18dbd4ca9cf70a2560d29
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-26 20:52:16 -0400

    Update WelcomeActivity.java

--- app/src/main/java/org/humhub/WelcomeActivity.java
+++ app/src/main/java/org/humhub/WelcomeActivity.java
@@ -56,7 +56,7 @@ public class WelcomeActivity extends AppCompatActivity {
     private void saveUrl(String url){
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("url",url);
-        editor.commit();
+        editor.apply();
     }
 
     private void fadeInImage(){
